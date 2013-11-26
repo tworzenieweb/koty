b8999fcc8217a594b355a8f9afda00bb63786008
tworzenieweb <tworzenieweb@gmail.com>
2013-11-26 19:06:16 +0100

    Added some plugins

--- wp-content/themes/cattery/content.php
+++ wp-content/themes/cattery/content.php
@@ -11,7 +11,7 @@
 
 <div class="span6" id="post-<?php the_ID(); ?>">
 
-    <h4 class="media-heading"><a href="<?php permalink_link() ?>"><?php the_title(); ?></a> <small class="label">Dodano <?php the_time('j F, Y') ?></small></h4>
+    <h4 class="media-heading"><a href="<?php permalink_link() ?>"><?php the_title(); ?></a> <small>Dodano <?php the_time('j F, Y') ?></small></h4>
                 <?php the_content( __( 'Kontynuuj <span class="meta-nav">&rarr;</span>', 'cattery' ) ); ?>
  
               <div class="divater"><div class="line"></div><span><img src="<?php bloginfo('template_url'); ?>/images/deviter.png" alt="" /></span></div>
